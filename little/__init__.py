a05bc8f0c40e5e23687a314ebba53f6a20ad91bd
wangjie <wangjie@juexiaotime.com>
2020-05-15 15:49:14 +0800

    little::__init__::update

--- little/__init__.py
+++ little/__init__.py
@@ -1,2 +1,10 @@
 # _*_ coding:utf-8 _*_
 # @Time 2020/5/11 9:20 下午
+from little.reutils.re_utils import ReLittle
+from little.datetimeutils.datetime_utils import DatetimeLittle
+
+
+relittle = ReLittle
+datetimelittle = DatetimeLittle
+
+__all__ =[relittle,datetimelittle]
